6ab68046139a4def1286f725796aba7cef1de4a8
Airam <airam.dev@gmail.com>
2022-01-21 02:53:05 +0000

    Added Mobile Menu + Responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,45 @@
+import {useState} from 'react';
+import Link from 'next/link';
 import '../styles/globals.css'
-import navStyles from "../styles/Nav.module.css";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
+import navStyles from '../styles/Nav.module.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
     <>
       <nav className={navStyles.nav}>
         <div className={navStyles.nav__box}>
           <img src="/logo.png" alt="Villa Ladridos"/>
-          <Link href="/" ><a className={navStyles.nav__title}>Villa Ladridos</a></Link>
+          <Link href="/"><a className={navStyles.nav__title}>Villa Ladridos</a></Link>
         </div>
-        <div className={navStyles.nav__box}>
+        <div className={`${navStyles.nav__box} ${navStyles.nav__links}`}>
           <Link href="/instalaciones">Instalaciones</Link>
           <Link href="/reservas">Reservas</Link>
           <Link href="/contacto">Contacto</Link>
         </div>
+        <div className={navStyles.nav__mobileButton} onClick={() => setMobileMenu(!mobileMenu)}>
+          <img src="/menu.png" alt="menu"/>
+        </div>
       </nav>
+      {
+        mobileMenu ? (
+          <div className={navStyles.nav__mobile}>
+            <Link href="/instalaciones">
+              <a onClick={() => setMobileMenu(false)}>Instalaciones</a>
+            </Link>
+            <Link href="/reservas" onClick={() => setMobileMenu(false)}>
+              <a onClick={() => setMobileMenu(false)}>Reservas</a>
+            </Link>
+            <Link href="/contacto" onClick={() => setMobileMenu(false)}>
+              <a onClick={() => setMobileMenu(false)}>Contacto</a>
+            </Link>
+          </div>
+        ) : (
+          ''
+        )
+      }
       <Component {...pageProps} />
     </>
   )
